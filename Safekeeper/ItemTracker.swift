1b577976aab739f5f62ae963b60959ce7c7ab7ef
Robin Johansson <robin_johans@outlook.com>
2016-08-15 23:32:50 +0200

    tagit bort testkod

--- Safekeeper/ItemTracker.swift
+++ Safekeeper/ItemTracker.swift
@@ -8,7 +8,6 @@ public class ItemTracker {
 	private (set) var trackedItems = [String:Item]()
 	private var trackEverything = false
 	private var timer: NSTimer!
-	static let NEARABLE_IDS = ["d27d8fc32418bb0c", "52908a26922d1b78", "9dd690633ac0e7f1", "cab715dbde99595b", "bcb42e39477d6c57", "30597ff274159387", "0aacb7671fd6dd88", "051f31577dca8d38", "a09e524a0618b78f", "47eb93db077a7890"]
 	
 	private init() {
 		trackerDelegate = ItemDelegate()
